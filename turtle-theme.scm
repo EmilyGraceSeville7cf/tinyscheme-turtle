7aca818313a6d76da0fa296637b4e4608608afcf
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-06-28 05:21:37 +1000

    feat: implement theming support

---
+++ turtle-theme.scm
@@ -0,0 +1,8 @@
+(define turtle-theme '((black (0 0 0))
+    (red (255 0 0))
+    (green (0 255 0))
+    (yellow (255 255 0))
+    (blue (0 0 255))
+    (magenta (255 0 138))
+    (cyan (2 247 243))
+    (gray (143 143 143))))
